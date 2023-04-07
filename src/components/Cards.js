e3fd656d7b1b70fe9d8ec6bf2c5c2f4ab80e66f2
Manuel Salazar <manusalh@gmail.com>
2023-04-07 19:45:06 -0400

    cards update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,68 +2,56 @@ import React from 'react'
 
 
 const Cards = () => {
+
+      let cards = [ 
+        {
+          img: "https://picsum.photos/200/300",
+         title: "Card title",
+         description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+         },
+         {
+          img: "https://picsum.photos/200/300",
+         title: "Card title",
+         description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+         },
+         {
+          img: "https://picsum.photos/200/300",
+         title: "Card title",
+         description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+         },
+         {
+          img: "https://picsum.photos/200/300",
+         title: "Card title",
+         description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+         }
+      ]
+  
     return (
 
       <div className="row  p-4 d-flex justify-content-center text-center">
-        <div className="card mx-2 col-3 p-0 my-2" style={{ width: "18rem" }}>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item p-0">
-              <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
-            </li>
-            <li className="list-group-item">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </li>
-          </ul>
-          <div className="card-footer">
-            <a href="#" className="btn btn-primary justify-content-center">Go somewhere</a>
-          </div>
-        </div>
+        
+          {cards.map(({img, title, description}, i) => {
+            return (
+              <div className="card mx-2 col-3 p-0 my-2" key={i} style={{ width: "18rem" }}>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item p-0">
+                  <img src={img} className="card-img-top" alt="..." />
+                </li>
+                <li className="list-group-item">
+                  <h5 className="card-title">{title}</h5>
+                  <p className="card-text">{description}</p>
+                </li>
+              </ul>
+              <div className="card-footer">
+                <a href="#" className="btn btn-primary justify-content-center">Go somewhere</a>
+              </div>
+            </div>
 
-        <div className="card col-3 p-0 mx-2 my-2" style={{ width: "18rem" }}>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item p-0">
-              <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
-            </li>
-            <li className="list-group-item">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </li>
-          </ul>
-          <div className="card-footer">
-            <a href="#" className="btn btn-primary justify-content-center">Go somewhere</a>
-          </div>
-        </div>
-
-        <div className="card col-3 p-0 mx-2 my-2" style={{ width: "18rem" }}>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item p-0">
-              <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
-            </li>
-            <li className="list-group-item">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </li>
-          </ul>
-          <div class="card-footer">
-            <a href="#" className="btn btn-primary justify-content-center">Go somewhere</a>
-          </div>
-        </div>
-
-        <div className="card col-3 p-0 mx-2 my-2" style={{ width: "18rem" }}>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item p-0">
-              <img src="https://picsum.photos/200/300" className="card-img-top" alt="..." />
-            </li>
-            <li className="list-group-item">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </li>
-          </ul>
-          <div className="card-footer">
-            <a href="#" className="btn btn-primary justify-content-center">Go somewhere</a>
-          </div>
-        </div>
+            )
+          
+            })}
+        
+    
 
       </div>
     
